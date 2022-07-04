f0f08efa5a9e5c5007130b7ea04d6474e477284e
PingchuanH <hi.imhpc@outlook.com>
2022-07-04 15:28:07 -0500

    Add new rules for MHRS

--- src/utils/Charm.js
+++ src/utils/Charm.js
@@ -1,26 +1,28 @@
-import Skill from './Skill.js';
-export default class Charm{
+export default class Charm {
     static RARE = 269484038;
     skill1;
     skill2;
     deco;
 
-    constructor(skill1, skill2) {
+    constructor(skill1, skill2, lv4DecoOverride = false) {
         this.skill1 = skill1;
         this.skill2 = skill2;
-        this.deco = this.maxDeco(skill1, skill2);
+        this.deco = this.maxDeco(skill1, skill2, lv4DecoOverride);
     }
 
-    maxDeco(skill1, skill2) {
-        if (skill1.rare == 'S' || skill1.rare == 'A' ||
-                skill2.rare == 'S' || skill2.rare == 'A') {
-            return [3,1,1];
+    maxDeco(skill1, skill2, lv4DecoOverride) {
+        if (lv4DecoOverride) {
+            return [4, 0, 0];
         }
-        return [3,2,1];
+        if (skill1.rare == 'S' || skill1.rare == 'A' ||
+            skill2.rare == 'S' || skill2.rare == 'A') {
+            return [3, 1, 1];
+        }
+        return [3, 2, 1];
     }
 
     toString(lang) {
-        if(this.skill1 == this.skill2) return this.skill1.getName(lang)+','+this.skill1.maxLvl[0]+','+','+0+','+this.deco[0]+','+this.deco[1]+','+this.deco[2];
-        return this.skill1.getName(lang)+','+this.skill1.maxLvl[0]+','+this.skill2.getName(lang)+','+this.skill2.maxLvl[1]+','+this.deco[0]+','+this.deco[1]+','+this.deco[2];
+        if (this.skill1 == this.skill2) return this.skill1.getName(lang) + ',' + this.skill1.maxLvl[0] + ',' + ',' + 0 + ',' + this.deco[0] + ',' + this.deco[1] + ',' + this.deco[2];
+        return this.skill1.getName(lang) + ',' + this.skill1.maxLvl[0] + ',' + this.skill2.getName(lang) + ',' + this.skill2.maxLvl[1] + ',' + this.deco[0] + ',' + this.deco[1] + ',' + this.deco[2];
     }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,36 @@
 <template>
     <div class="home">
-        <a target="_blank" rel="noopener noreferrer" :href="'https://mhrise.wiki-db.com/sim/?hl='+$store.state.lang">配装器链接 (Armorset Search )</a>
+        <h2 style="text-align: center; border: none">
+            <a
+                target="_blank"
+                rel="noopener noreferrer"
+                :href="
+                    'https://mhrise.wiki-db.com/sim/?hl=' + $store.state.lang
+                "
+                >配装器链接 (Go to Armorset Search)</a
+            >
+        </h2>
+        <div>
+            <p class="inline">Select Melding Type:</p>
+            <select v-model="$store.state.meldingCate" class="inline">
+                <!-- <option :label="getMeldingText(0)">0</option> -->
+                <!-- <option :label="getMeldingText(1)">1</option> -->
+                <option label="MHRise">0</option>
+                <option label="MHRise SB">1</option>
+            </select>
+        </div>
+        <div v-if="$store.state.meldingCate == 1">
+            <input
+                type="checkbox"
+                id="lv4Deco"
+                v-model="$store.state.enableLv4Deco"
+            />
+            <label for="lv4Deco"> Use Lv.4 Deco</label><br />
+        </div>
+
         <div class="skillSelector">
             <button
-                v-for="skill in $store.state.skills"
+                v-for="skill in $store.state.skills[$store.state.meldingCate]"
                 :class="['skill', skill.selected]"
                 :key="skill.id"
                 :selected="skill.selected"
@@ -15,40 +42,62 @@
         <div>
             <button class="gen" @click="generateCharms">Copy Result</button>
         </div>
-        <p v-for="val in output" :key="val"> {{val.toString(this.$store.state.lang)}} </p>
+        <p v-for="val in output" :key="val">
+            {{ val.toString(this.$store.state.lang) }}
+        </p>
     </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Charm from "@/utils/Charm.js";
+import { meldingCates_name as zh_hans } from "../utils/lang/zh-hans.js";
+import { meldingCates_name as en } from "../utils/lang/en.js";
 
 export default {
     name: "Home",
     data() {
         return {
             output: [],
+            meldingCateTexts: {
+                en: en,
+                "zh-hans": zh_hans,
+            },
         };
     },
     methods: {
         generateCharms() {
+            console.log("genCharms...");
             this.output = [];
-            for (let skill1 of this.filterSelectedSkills) {
-                for (let skill2 of this.filterSelectedSkills) {
-                    if (skill1 == skill2){
-                        if (this.filterSelectedSkills.length != 1) continue;
+            const loopCharms = (lv4Deco=false) => {
+                for (let skill1 of this.filterSelectedSkills) {
+                    for (let skill2 of this.filterSelectedSkills) {
+                        if (skill1 == skill2) {
+                            if (this.filterSelectedSkills.length != 1) continue;
+                        }
+                        if (skill1.id > skill2.id) {
+                            if (
+                                skill1.maxLvl[0] == skill1.maxLvl[1] &&
+                                skill2.maxLvl[0] == skill2.maxLvl[1]
+                            )
+                                continue;
+                        }
+                        let charm = new Charm(
+                            skill1,
+                            skill2,
+                            lv4Deco
+                        );
+                        this.output.push(charm);
                     }
-                    if(skill1.id > skill2.id) {
-                        if (skill1.maxLvl[0]==skill1.maxLvl[1] && skill2.maxLvl[0]==skill2.maxLvl[1]) continue;
-                    }
-                    let charm = new Charm(skill1, skill2);
-                    this.output.push(charm);
                 }
             }
-            
+            loopCharms();
+            if (this.$store.state.enableLv4Deco) {
+                loopCharms(true);
+            }
             // to clipboard
             let str = "";
-            for(let item of this.output){
+            for (let item of this.output) {
                 str += item.toString(this.$store.state.lang);
                 str += "\n";
             }
@@ -66,10 +115,16 @@ export default {
                 }
             );
         },
+
+        getMeldingText(id) {
+            return this.meldingCateTexts[this.$store.state.lang][id];
+        },
     },
     computed: {
         filterSelectedSkills() {
-            return this.$store.state.skills.filter((skill) => skill.selected);
+            return this.$store.state.skills[
+                this.$store.state.meldingCate
+            ].filter((skill) => skill.selected);
         },
     },
 };
@@ -119,5 +174,9 @@ button:active {
     border-bottom: 1px;
     border-bottom-style: dashed;
     border-bottom-color: #8f9396;
+}
+
+.inline {
+    display: inline-block;
 }
 </style>
